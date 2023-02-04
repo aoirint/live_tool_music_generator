58b86d4ecc6a52c50cc9c4b5d7f3f9c6a53b47b2
aoirint <aoirint@gmail.com>
2023-02-04 17:27:47 +0900

    load music metadata (ID3v1.1 only)

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from pathlib import Path
 from dataclasses import dataclass
-from aoirint_id3 import detect_id3_versions
+from aoirint_id3 import detect_id3_versions, decode_id3v1_1
+from aoirint_id3.id3v1_1 import available_genres
+from typing import Optional
+
+genre_dict = dict(map(lambda genre: (genre.genre_id, genre.genre_name), available_genres))
 
 
 @dataclass
 class MusicMetadata:
-  name: str
-  artist: str
-  album: str
-  genre: str
-  year: str
+  name: Optional[str]
+  artist: Optional[str]
+  album: Optional[str]
+  genre: Optional[str]
+  year: Optional[str]
 
 
 @dataclass
@@ -25,18 +29,42 @@ class Playlist:
   musics: list[Music]
 
 
-def load_metadata(music_file: Path) -> MusicMetadata:
-  raise Exception('Unimplemented')
-
-
 def load_music(music_file: Path) -> Music:
-  raise Exception('Unimplemented')
+  music_bytes = music_file.read_bytes()
+
+  name: Optional[str] = None
+  artist: Optional[str] = None
+  album: Optional[str] = None
+  genre: Optional[str] = None
+  year: Optional[str] = None
+
+  id3_versions = detect_id3_versions(data=music_bytes)
+  if 'ID3v1.1' in id3_versions:
+    result = decode_id3v1_1(data=music_bytes[-128:], encoding='latin-1')
+    name = result.title
+    artist = result.artist
+    album = result.album
+    genre_number = result.genre_number
+    genre = genre_dict.get(genre_number)
+    year = result.year
+
+  return Music(
+    filename=music_file.name,
+    metadata=MusicMetadata(
+      name=name,
+      artist=artist,
+      album=album,
+      genre=genre,
+      year=year,
+    ),
+    duration_millis=0,
+  )
 
 
 def load_playlist(playlist_dir: Path) -> Playlist:
   music_files = list(filter(lambda path: path.is_file(), playlist_dir.iterdir()))
 
-  musics = list[Music]
+  musics: list[Music] = []
   for music_file in music_files:
     musics.append(load_music(music_file=music_file))
   
@@ -65,8 +93,10 @@ def main():
   playlists_dir = Path('playlists')
   playlists_dir.mkdir(parents=True, exist_ok=True)
 
-  load_playlists(
-    playlists_dir=playlists_dir,
+  print(
+    load_playlists(
+      playlists_dir=playlists_dir,
+    )
   )
 
 

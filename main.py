b8773aa74287b35a8e9eb2c04ea70495a286f0f7
aoirint <aoirint@gmail.com>
2023-02-08 18:34:54 +0900

    attached_picture

--- main.py
+++ main.py
@@ -8,13 +8,21 @@ genre_dict = dict(map(lambda genre: (genre.genre_id, genre.genre_name), availabl
 
 
 @dataclass
+class AttachedPicture:
+    mime_type: str
+    picture_type: int
+    description: str
+    picture_data: bytes
+
+
+@dataclass
 class MusicMetadata:
   name: Optional[str]
   artist: Optional[str]
   album: Optional[str]
   genre: Optional[str]
   year: Optional[str]
-
+  attached_picture: Optional[AttachedPicture]
 
 @dataclass
 class Music:
@@ -37,6 +45,7 @@ def load_music(music_file: Path) -> Music:
   album: Optional[str] = None
   genre: Optional[str] = None
   year: Optional[str] = None
+  attached_picture: Optional[AttachedPicture] = None
 
   id3_versions = detect_id3_versions(data=music_bytes)
   if 'ID3v2.3' in id3_versions:
@@ -46,6 +55,12 @@ def load_music(music_file: Path) -> Music:
     album = result.album
     # TODO: genre
     year = result.year
+    attached_picture = AttachedPicture(
+      mime_type=result.attached_picture.mime_type,
+      picture_type=result.attached_picture.picture_type,
+      description=result.attached_picture.description,
+      picture_data=result.attached_picture.picture_data,
+    )
   elif 'ID3v2.2' in id3_versions:
     result = decode_id3v2_2(data=music_bytes)
     name = result.title
@@ -78,6 +93,7 @@ def load_music(music_file: Path) -> Music:
       album=album,
       genre=genre,
       year=year,
+      attached_picture=attached_picture,
     ),
     duration_millis=0,
   )
@@ -115,11 +131,21 @@ def main():
   playlists_dir = Path('playlists')
   playlists_dir.mkdir(parents=True, exist_ok=True)
 
-  print(
-    load_playlists(
-      playlists_dir=playlists_dir,
-    )
+  playlists = load_playlists(
+    playlists_dir=playlists_dir,
   )
+
+  def write_first_artwork_to_file():
+    for playlist in playlists:
+      for music in playlist.musics:
+        if music.metadata.attached_picture is not None:
+          with open('a.png', 'wb') as fp:
+            fp.write(music.metadata.attached_picture.picture_data)
+            return
+
+  write_first_artwork_to_file()
+
+  print(playlists)
 
 
 if __name__ == '__main__':
